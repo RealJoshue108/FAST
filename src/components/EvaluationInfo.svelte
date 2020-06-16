57cac8c0d34795c2f173d6db95ff89df1e56841d
RealJoshue108 <joconnor@w3.org>
2020-06-16 16:20:31 +0100

    Evaluation reporting string change to FAST checkpoint

--- src/components/EvaluationInfo.svelte
+++ src/components/EvaluationInfo.svelte
@@ -59,7 +59,7 @@
       {/if}
     </h2>
     {#if evaluatedItems && $evaluation.evaluationData}
-    <p>Reported on <strong>{evaluatedItems.length}</strong> out of <strong>{totalCriteria}</strong> success criteria.</p>
+    <p>Reported on <strong>{evaluatedItems.length}</strong> out of <strong>{totalCriteria}</strong> FAST checkpoint.</p>
     {/if}
     <button class="button" on:click={toOverview}>View report</button>
     <button class="button button-secondary" on:click={clear}>Clear</button>
